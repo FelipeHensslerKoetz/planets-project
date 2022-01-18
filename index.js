0764969a36eee9075bc0afc4b4eaab8d45bbe07c
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-01-17 22:10:50 -0300

    chore: add habitable planets filter

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 const results = [];
 
+function isHabitablePanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
 fs.createReadStream("kepler_data.csv")
   .pipe(
     parse({
@@ -10,12 +19,14 @@ fs.createReadStream("kepler_data.csv")
     })
   )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePanet(data)) {
+      results.push(data);
+    }
   })
   .on("error", (error) => {
     console.log(error);
   })
   .on("end", () => {
     console.log(results);
-    console.log("Done!");
+    console.log(`${results.length} habitable planets found!`);
   });
